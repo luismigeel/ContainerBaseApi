b066cc5998f56ebc84f190c8c7b9abf239de02ea
Luis Miguel Zavala Felix <zavalafelix.luismiguel@gmail.com>
2022-05-14 22:43:33 -0600

    agg; helloworld & redirect to docs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
-from fastapi import HTTPException
+from starlette.responses import RedirectResponse
+
 
 app = FastAPI(
     title="Template",
-    version="0.0.1"
+    version="0.0.1",
+    openapi_url="/openapi.json"
     )
 
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+@app.get(path='/',
+         summary="index for template",
+         tags=["index"])
+async def index():
+    """
+    It redirects the user to the /docs page.
+    :return: RedirectResponse is a class from starlette.responses
+    """
+    return RedirectResponse("/docs")
